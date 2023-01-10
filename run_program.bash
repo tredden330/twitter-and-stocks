0691ff6241b5fae1c66b18eec9a0089e6c0257bf
tredden330 <tom@thomasredden.com>
2023-01-10 15:19:24 -0500

    added JSON pretty print

--- run_program.bash
+++ run_program.bash
@@ -1,15 +1,15 @@
-numTweets="26"
+numTweets="99"
 user="elonmusk"
 
 
 fullURL=https://api.twitter.com/2/tweets/search/recent?query=from:$user"&max_results="$numTweets"&tweet.fields=public_metrics,created_at"
 
-curl $fullURL --header 'Authorization: Bearer '$BEARER_TOKEN > results.json
+curl $fullURL --header 'Authorization: Bearer '$BEARER_TOKEN > elon_tweets.json
 
-echo $BEARER_TOKEN
+#echo $BEARER_TOKEN
 
-jq . results.json
+wait
 
-#jq .id results.json > id_results.json
+jq . elon_tweets.json > elon_tweets_pretty.json
 
 
