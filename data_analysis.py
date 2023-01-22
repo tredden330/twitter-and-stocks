4a0a0c3df6174ca1f56e7f2c0f2e0c7779c4602a
tredden330 <tom@thomasredden.com>
2023-01-22 17:53:35 -0500

    added twitter data plotting

--- data_analysis.py
+++ data_analysis.py
@@ -5,31 +5,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def readJSON():
-	file = open("results.json")
+	file = open("elon_tweets.json")
 
 	data = json.load(file)
 	x = pd.DataFrame()
 
 	for i in data['data']:
-		print('id: ', i['id'],'date: ', i['created_at'], 'likes: ', i['public_metrics']['like_count'],'impressions: ', i['public_metrics']['impression_count'])
-		x = x.append({"id" : i['id'], "created_at" : i['created_at'], "likes" : i['public_metrics']['like_count'],'impressions' : i['public_metrics']['impression_count']}, ignore_index = True)
+#		print('id: ', i['id'],'date: ', i['created_at'], 'likes: ', i['public_metrics']['like_count'],'impressions: ', i['public_metrics']['impression_count'])
+		row_vector = pd.DataFrame([[i['id'], i['created_at'], i['public_metrics']['like_count'], i['public_metrics']['impression_count']]], columns=['id', 'date', 'likes', 'impressions'])
+		x = pd.concat([x, row_vector])
 	file.close()
 
-	print(x)
+	dates = x.loc[:, 'date']
+	likes = x.loc[:, 'likes']
+
+
+	fig, ax = plt.subplots()
+	ax.plot(dates, likes)
+
+	every_nth = 20
+	for n, label in enumerate(ax.xaxis.get_ticklabels()):
+		if n % every_nth != 0:
+			label.set_visible(False)
+
+	plt.show()
+
+	print(likes)
 
 def fetchStockPrices():
-	start_date = '2023-01-01'
-	end_date = '2023-01-07'
 
-	# Set the ticker
-	ticker = 'TSLA'
-	# Get the data
-	data = yf.download(ticker, start_date, end_date)
-	# Print the last 5 rows
-	print(data)
+	tsla = yf.Ticker('TSLA')
+
+	hist = tsla.history(period='max')
+
+	data = pd.DataFrame(hist)
+	print(data.index)
+
+	x = data.index
+	y = data.loc[:,'Open']
+
+	fig, ax = plt.subplots()
+	ax.plot(x, y)
+
+	plt.show()
 
 def graphTrend():
-	print("hi")
+
 	x = np.linspace(0, 10, 100)
 	y = 4 + 2 * np.sin(2 * x)
 
